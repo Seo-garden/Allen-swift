0e8284348b885f45664222a137e6c70b6bc06118
seo-garden <indian4589@naver.com>
2024-01-19 16:53:34 +0900

    0119 commit

--- part11-1/MyPlayground.playground/Contents.swift
+++ part11-1/MyPlayground.playground/Contents.swift
@@ -14,3 +14,33 @@ class Dog {
     }
 }
 var bori = Dog(name: "보리", weight: 10)
+
+class Person {
+    var height : Double
+    var weight : Double
+    init(height: Double, weight: Double) {
+        self.height = height
+        self.weight = weight
+    }
+    func calculateBMI() -> Double{
+        let bmi = weight / (height * height) * 10000
+        return bmi
+    }
+}
+let a = Person(height: 160, weight: 60)
+a.calculateBMI()
+
+class Person1 {
+    var height : Double
+    var weight : Double
+    init(height: Double, weight: Double) {
+        self.height = height
+        self.weight = weight
+    }
+    var bmi : Double{
+        var result = weight / (height * height) * 10000
+        return result
+    }
+}
+let b = Person1(height: 160, weight: 60)
+b.bmi
